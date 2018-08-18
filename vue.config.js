010021508236610e3abb0d98cd853ed9de04e39e
Dan Behlings <nevenall@hotmail.com>
2018-08-18 15:43:15 -0700

    fixed a couple paths

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
          favicon32: 'icons/favicon-32x32.png',
          favicon16: 'icons/favicon-16x16.png',
          appleTouchIcon: 'icons/apple-touch-icon.png',
-         maskIcon: 'img/icons/safari-pinned-tab.svg',
+         maskIcon: 'icons/safari-pinned-tab.svg',
          msTileImage: 'icons/mstile-150x150.png'
       }
    },
